ded2b5b8900fc6fd83fc6fc816a921c0fecdc316
barakaally <baraka.ally.co.tz@gmail.com>
2022-01-25 23:17:20 +0300

    added maping of be2unlayer

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import { BeefreeDesign } from "../src/model/beefree.model";
 import {UnlayerDesign} from '../src/model/unlayer.model'
 
-export declare class UnlayerEmailJson{
+export declare class Unlayer2be{
     static from(data:BeefreeDesign|string):UnlayerDesign
 }
