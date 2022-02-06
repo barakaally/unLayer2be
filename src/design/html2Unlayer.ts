b5019aa95bd4f4973ab99097ddaf4e0e37333d0c
barakaally <baraka.ally.co.tz@gmail.com>
2022-02-06 21:01:44 +0300

    #fixed unlayer html button

--- src/design/html2Unlayer.ts
+++ src/design/html2Unlayer.ts
@@ -1,3 +1,4 @@
+import { JSDOM } from "jsdom";
 import { UnlayerDesign } from "../model/unlayer.model";
 import { HtmlParser } from "../utils/htmlParser";
 
@@ -7,7 +8,7 @@ export class Html2Unlayer {
 
         let design = {} as UnlayerDesign;
         const body = HtmlParser.parseBody(data).querySelector("body");
-
+    
         design.body = {
 
             rows: HtmlParser.parseRows(body).map((row: any, i) => {
@@ -50,11 +51,11 @@ export class Html2Unlayer {
     });
 
     getContents = (column: any) => Array.from(column.children).map((content: any, i) => {
-
+        const type=this.getContentType(content);
         return {
-            type: this.getContentType(content),
+            type: type,
             values: {
-                ...this.getStyle(content.style, content.outerHTML, `u_content_${this.getContentType(content)}_${i + 1}`) as any,
+                ...this.getStyle(type==="button"?content.style._values:content.style, type==="button"?content?.lastElementChild.innerHTML:content.outerHTML, `u_content_${type}_${i + 1}`) as any,
                 ...{
                     src: {
                         url: content?.querySelector("img")?.src,
