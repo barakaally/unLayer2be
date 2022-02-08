0e1243a60d4727069c3b5abb7f769404c02f16ec
barakaally <baraka.ally.co.tz@gmail.com>
2022-02-08 17:29:44 +0300

    added postinstall

--- script/install-jsdom.ts
+++ script/install-jsdom.ts
@@ -3,9 +3,8 @@ import ps from 'child_process';
 
 runOnBrowser(run => {
     if (!run) {
-        console.log("[*] Installing jsdom");
-        const out = ps.exec("npm install jsdom@19.0.0");
-        out.stdout.pipe(process.stdout);
-        out.stderr.pipe(process.stderr);
+        const res = ps.exec("npm install jsdom@19.0.0");
+        res.stdout.pipe(process.stdout);
+        res.stderr.pipe(process.stderr);
     }
 })
