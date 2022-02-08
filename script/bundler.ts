1ef2cb13f0ee56894366a2c91fa927dbb51afd66
barakaally <baraka.ally.co.tz@gmail.com>
2022-02-08 12:23:44 +0300

    updated runOnBrowser

--- script/bundler.ts
+++ script/bundler.ts
@@ -2,12 +2,12 @@ import ps from 'child_process';
 const bundlers = ["webpack", "browserify", "rollup", "esbuild", "parcel", "babel"];
 
 export function runOnBrowser(callback: (run: boolean) => void) {
-    const res = ps.exec(`npm list ${bundlers.join(",").replace(/\,/g, " ")}`);
+    const res = ps.exec(`cd../&&npm list ${bundlers.join(",").replace(/\,/g, " ")}`);
     let result = "";
     res.stdout.on("data", (data) => {
         result += data;
     });
-
+    
     res.stdout.on("end", () => {
         callback(new RegExp(`(${bundlers.join("|")})`, "gi").test(`${result.replace(/\`/g,"")}` ));
     })
